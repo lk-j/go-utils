235332dacf9f444ac0175272381eeb2aea083195
lk-j <381584494@qq.com>
2022-11-05 11:04:24 +0800

    upload file edit

--- http/file_upload.go
+++ http/file_upload.go
@@ -2,7 +2,6 @@ package http
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"mime/multipart"
@@ -10,29 +9,27 @@ import (
 	"os"
 )
  //模拟客户端上传文件
-func PostFile(filename string, targetUrl string) error {
+func PostFile(filename string, targetUrl string) (string,error) {
 	bodyBuf := &bytes.Buffer{}
 	bodyWriter := multipart.NewWriter(bodyBuf)
 
 	//关键的一步操作
 	fileWriter, err := bodyWriter.CreateFormFile("uploadfile", filename)
 	if err != nil {
-		fmt.Println("error writing to buffer")
-		return err
+		return "",err
 	}
 
 	//打开文件句柄操作
 	fh, err := os.Open(filename)
 	if err != nil {
-		fmt.Println("error opening file")
-		return err
+		return "",err
 	}
 	defer fh.Close()
 
 	//iocopy
 	_, err = io.Copy(fileWriter, fh)
 	if err != nil {
-		return err
+		return "",err
 	}
 
 	contentType := bodyWriter.FormDataContentType()
@@ -40,14 +37,12 @@ func PostFile(filename string, targetUrl string) error {
 
 	resp, err := http.Post(targetUrl, contentType, bodyBuf)
 	if err != nil {
-		return err
+		return "",err
 	}
 	defer resp.Body.Close()
 	resp_body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return err
+		return "",err
 	}
-	fmt.Println(resp.Status)
-	fmt.Println(string(resp_body))
-	return nil
+	return string(resp_body),nil
 }
